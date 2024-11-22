35b6c902ae0fc13e90c48ee27559d3959d28b701
AbdulBasit <basitabdul445@gmail.com>
2024-11-22 12:22:50 +0500

    added server-side validation for Create Category

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -9,8 +9,10 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(20,ErrorMessage ="Length shoud not exceed 20 characters.")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,int.MaxValue,ErrorMessage = "Display Order must be between 1-2147483647")]
         public int DisplayOrder { get; set; }
     }
 }
